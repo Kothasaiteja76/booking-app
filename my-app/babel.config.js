5b05b2e7cfde9103dddf545d72e25fe77eea58fd
kothasaiteja76 <kothasaiteja76@gmail.com>
2026-03-07 16:54:03 +0530

    my-setup

---
+++ my-app/babel.config.js
@@ -0,0 +1,19 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['dist/*'],
+  },
+]);
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
+};
